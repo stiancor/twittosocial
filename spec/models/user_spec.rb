cad07df94fada2618a9a5977feeb043c35840a1e
Stian Alsos <stian.alsos@gmail.com>
2012-10-16 11:39:41 +0200

    Fix for regex on email addresses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,7 +33,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@foo+bar.com foo@foo_bar.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         @user.should_not be_valid
