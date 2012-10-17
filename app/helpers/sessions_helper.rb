a95a8a3d5667b6e214e105036e15b704ad9244fd
Stian Alsos <stian.alsos@gmail.com>
2012-10-17 13:16:42 +0200

    Correct error in sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    current_user = user
+    self.current_user = user
   end
 
   def signed_in?
@@ -18,7 +18,7 @@ module SessionsHelper
   end
 
   def sign_out
-    current_user = nil
+    self.current_user = nil
     cookies.delete :remember_token
   end
 end
