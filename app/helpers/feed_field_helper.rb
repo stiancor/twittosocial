46a29e032cb83b1adb131ab67f4f2bfee2cc31f4
Stian Alsos <stian.alsos@gmail.com>
2012-10-23 22:17:08 +0200

    Escaping tags if present, and opening links in new tab

--- app/helpers/feed_field_helper.rb
+++ app/helpers/feed_field_helper.rb
@@ -1,15 +1,28 @@
 module FeedFieldHelper
   def format_msg(content)
+    if !content.match /<|>/
+      create_links_if_present(content)
+    else
+      escape_html(content)
+    end
+  end
+
+  def create_links_if_present(content)
     i = 0
     returnArray = []
     content.split(' ').each do |s|
       if s.match /http:\/\/.*?/
-        returnArray[i] = "<a href='#{s}'>#{s}</a>"
+        returnArray[i] = "<a href='#{s}' target='_blank'>#{s}</a>"
       else
         returnArray[i] = s
       end
       i += 1
     end
     returnArray.join(' ')
+  end
+
+  def escape_html(content)
+    content.gsub!('<', '&lt;')
+    content.gsub!('>', '&gt;')
   end
 end
