d0f1f8ca489db95e57fa2c89609a18d3b342605d
Stian Alsos <stian.alsos@gmail.com>
2012-11-28 22:23:36 +0100

    Adding test that should have been there. Ready for some refactoring

--- spec/helpers/feed_field_helper_spec.rb
+++ spec/helpers/feed_field_helper_spec.rb
@@ -14,12 +14,36 @@ describe FeedFieldHelper do
       it { @micropost.scan(/<.*?>.*?\>/)[0].should == "<a href='https://www.twittosocial.com' target='_blank'>https://www.twittosocial.com</a>" }
     end
 
-    describe "should find all links" do
-      before do @micropost = parse_words_one_by_one("First https://www.google.com second http://yahoo.com")
+    describe "should find both links" do
+      before do
+        @micropost = parse_words_one_by_one("First https://www.google.com second http://yahoo.com")
         @links = @micropost.scan(/<.*?>.*?\>/)
       end
       it { @links[0].should == "<a href='https://www.google.com' target='_blank'>https://www.google.com</a>" }
       it { @links[1].should == "<a href='http://yahoo.com' target='_blank'>http://yahoo.com</a>" }
+    end
+
+    describe "links without http" do
+      describe "link starting with www should be wrapped with <a> tags" do
+        before { @micropost = parse_words_one_by_one("This is a www.twittosocial.com link") }
+        it { @micropost.scan(/<.*?>.*?\>/)[0].should == "<a href='http://www.twittosocial.com' target='_blank'>www.twittosocial.com</a>" }
+      end
+    end
+
+    describe "links with and without http prefix" do
+      describe "should be wrapped with <a> tags" do
+        before do
+          @micropost = parse_words_one_by_one("This is a www.twittosocial.com link and this http://www.twittosocial.com that has http in it")
+          @links = @micropost.scan(/<.*?>.*?\>/)
+        end
+        it { @links[0].should == "<a href='http://www.twittosocial.com' target='_blank'>www.twittosocial.com</a>" }
+        it { @links[1].should == "<a href='http://www.twittosocial.com' target='_blank'>http://www.twittosocial.com</a>" }
+      end
+    end
+
+    describe "escape tags in message" do
+      before { @micropost = escape_html("This <script>alert('Twittosocial rocks');</script>") }
+      it { @micropost.should == "This &lt;script&gt;alert('Twittosocial rocks');&lt;/script&gt;" }
     end
 
   end
