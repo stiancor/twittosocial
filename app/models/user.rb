879bccc3063e0bb557a4a84e3e87dbecaf823fdd
Stian Alsos <stian.alsos@gmail.com>
2013-01-25 21:26:45 +0100

    Downcasing username before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ActiveRecord::Base
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
 
-  before_save { |user| user.email = user.email.downcase }
+  before_save { |user| user.email = user.email.downcase
+                       user.username = user.username.downcase}
   before_save :create_remember_token
 
   validates :name, presence: true, length: {maximum: 50}
