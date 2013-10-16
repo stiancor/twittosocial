e54f14bb8c9567e146f808643165c6c4632976a8
Stian Alsos <stian.alsos@gmail.com>
2013-10-17 00:07:29 +0200

    Only showing notifications for new events in mobile view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,13 +11,24 @@ module ApplicationHelper
   end
 
   def events_summary
-    @event_summary = Event.includes(:event_invites)
-    .where('end_time > ? and event_invites.user_id = ?', DateTime.now, current_user.id)
-    .count(:group => 'attend_status')
+    fetch_event_summary
     "Events (#{unanswered_invites}/#{probably_attending}/#{total_upcoming_events})"
   end
 
+  def event_mobile_summary
+    fetch_event_summary
+    if @event_summary['no_reply']
+      @event_summary['no_reply'] ? "<span class='badge badge-important'>#{@event_summary['no_reply'].to_i}</span>" : ''
+    end
+  end
+
   private
+
+  def fetch_event_summary
+    @event_summary = Event.includes(:event_invites)
+    .where('end_time > ? and event_invites.user_id = ?', DateTime.now, current_user.id)
+    .count(:group => 'attend_status')
+  end
 
   def total_upcoming_events
     @event_summary['no_reply'].to_i + @event_summary['no'].to_i + @event_summary['yes'].to_i + @event_summary['maybe'].to_i
