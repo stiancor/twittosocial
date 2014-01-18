e685bd5a7c0d0cb1de28f9830743c97ed7694b98
stian.alsos@gmail.com <stian.alsos@gmail.com>
2014-01-18 21:02:37 +0100

    Added some tests for the event page

---
+++ spec/requests/event_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "EventPages" do
+
+  subject { page }
+
+  let(:user) { FactoryGirl.create(:user) }
+
+  describe "not signed in" do
+    before { visit events_path }
+    it { should have_link("Sign in", href: signin_path) }
+  end
+
+  describe "with signed in user" do
+    before { sign_in user }
+
+    describe "Should get list of event" do
+      before { visit events_path }
+      it do
+        should have_selector('h1', text: 'All upcoming events')
+        should have_link 'Create new event'
+      end
+    end
+  end
+
+end
