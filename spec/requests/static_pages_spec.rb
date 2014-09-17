64bcaa2ba4144471803dbe7a8a5fcc44d9d81994
stian.alsos@gmail.com <stian.alsos@gmail.com>
2014-09-17 22:40:12 +0200

    Fixing test. No longer a "Welcome" text in h1

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,6 @@ describe "Static pages" do
     before {
       visit root_path
     }
-    it { should have_selector('h1', text: 'Welcome') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector('title', text: '| Home') }
 
