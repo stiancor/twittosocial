d44dd5d67861f755e0e4955cd1d1d5e0d4985d79
stian.alsos@gmail.com <stian.alsos@gmail.com>
2014-09-17 22:56:56 +0200

    New tests on admin message functionality

--- spec/helpers/micropost_helper_spec.rb
+++ spec/helpers/micropost_helper_spec.rb
@@ -3,34 +3,18 @@ require "spec_helper"
 
 describe MicropostHelper do
 
-  let(:user) { FactoryGirl.create(:user) }
-
-  before do
-    User.create!(name: "Example user",
-                 email: "example@example.com",
-                 username: "someuser",
-                 password: "foobar",
-                 password_confirmation: "foobar")
+  describe "return admin-message if micropost is marked with admin" do
+    before {
+      @micropost = Micropost.new(content: 'Some content', admin_message: true)
+    }
+    it { micropost_admin_class(@micropost) == 'admin-message'  }
   end
 
-  describe "only matches if username exists" do
-    it { get_emails(['someuser', 'nouser']).length.should == 1 }
-  end
-
-  describe "extract usernames from micropost" do
-    before { @user_names = extract_user_names("@stiancor, this is true, but @somebody, meaning @nobody does not agree! @") }
-    it { @user_names.length.should == 3 }
-    it { @user_names[0].should == "stiancor" }
-    it { @user_names[1].should == "somebody" }
-    it { @user_names[2].should == "nobody" }
-  end
-
-  describe "no usernames in string without usernames" do
-    it { extract_user_names("This string contains no usernames @, that is for sure").length.should == 0 }
-  end
-
-  describe "micropost with three usernames" do
-    it { extract_user_names("Vi planlegger julebordet og er usikker på om @benty @chaub og @marie liker pinnekjøtt... Yes or no").length.should == 3 }
+  describe "return nothing if micropost is not marked with admin" do
+    before {
+      @micropost = Micropost.new(content: 'Some content', admin_message: false)
+    }
+    it { micropost_admin_class(@micropost) == '' }
   end
 
 end
